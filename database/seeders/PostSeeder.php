8f65299bd3cc9f3d2e617827fe792e7445c2b2a2
Clionegohan <sawakiguchik@gmail.com>
2024-08-19 14:37:41 +0900

    カテゴリー作成しました。投稿画面、詳細画面、カテゴリ別、編集画面も簡単ではあるが。

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Datetime;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('posts')->insert([
+            'user_id' => 2,
+            'category_id' => 7,
+            'title' => '虹の実',
+            'body' => '気温や湿度によって七色に味を変え、口にすると体内で味が次々に変化し、食べたものに大きな感動を与える。
+                       25メートルプールの水に果汁をたった1滴たらすだけでプールの水全てが芳醇なジュースに変わるほどの高い果汁濃度を
+                       持ち、蒸発した果汁は空気中に虹を作る。',
+            'address' => '第8ビオトープ',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            
+            ]);
+    }
+}
