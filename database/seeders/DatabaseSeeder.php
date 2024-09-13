e14d3e43a8ee9aa693e92a749e8a90c9809ddcbb
Clionegohan <sawakiguchik@gmail.com>
2024-09-13 15:55:40 +0900

    database.seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            //CategorySeeder::class,
-            //UserSeeder::class,
-            //PostSeeder::class,
+            CategorySeeder::class,
+            UserSeeder::class,
+            PostSeeder::class,
             ]);
         
         
