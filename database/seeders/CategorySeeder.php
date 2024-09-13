f579662580df28e3ab15dfa7ec5445c88f1a6415
Clionegohan <sawakiguchik@gmail.com>
2024-09-13 16:18:54 +0900

    categories.seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,16 +14,17 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
+        // 英語のカテゴリ名
         $categories = ['starter', 'soup', 'fish', 'meat', 'main', 'salad', 'dessert', 'drink'];
-        
+
+        // 日本語のカテゴリ名
         $categories_jp = ['オードブル(前菜)', 'スープ', '魚料理', '肉料理', '主菜(メイン)', 'サラダ', 'デザート', 'ドリンク'];
-        
+
+        // カテゴリ名と日本語名をまとめて挿入
         DB::table('categories')->insert(
-            array_map(
-                fn($name, $name_jp) => ['name' =>$name, 'name_jp' => $name_jp],
-                $categories,
-                $categories_jp
-                )
-            );
+            array_map(function ($name, $name_jp) {
+                return ['name' => $name, 'name_jp' => $name_jp];
+            }, $categories, $categories_jp)
+        );
     }
 }
