6bf15865480d8ee117a5e1c035fbda88eddbd71d
Clionegohan <sawakiguchik@gmail.com>
2024-09-14 12:23:59 +0900

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP" rel="stylesheet">
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
         <!-- 子テンプレートで追加可能なCSS -->
         @yield('head')
         
